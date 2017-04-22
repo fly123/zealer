ab69c0c17f88a3705b09efc960e32fb621337fc5
ubuntu <1196722167@qq.com>
2017-04-22 19:55:46 +0800

    edit

--- crawler/script/start.sh
+++ crawler/script/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-WORK_DIR=~/monitor/alive
+WORK_DIR=~/jiafei/zealer/crawler/
 
 cd ${WORK_DIR}
 
-TARGET="client.py luojiafei"
+TARGET="zealer.py"
 result=`ps -ewf|grep "$TARGET"|grep -wv grep|grep -wv vi | grep -wv tail | grep -wv check_alive | grep -v "\.sh" | grep -v "/alarm " |grep -v "vim"| wc -l `
 echo $result
 
@@ -11,9 +11,9 @@ echo $result
 NOW_TIME=`date +'%Y-%m-%d %H:%M:%S'`
 if [ "$result" -eq "0"  ]
 then
-    echo "start $TARGET $NOW_TIME" >> log/client_down.log
-    script/kill_client.sh
-    nohup /usr/local/Cellar/python/2.7.12_2/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python $TARGET >> log/client_log.txt 2>&1 &
+    echo "start $TARGET $NOW_TIME" > log/zealer.log
+    script/kill.sh
+    nohup python $TARGET > log/zealer.log 2>&1 &
 else
     echo "" #""$TARGET exists $NOW_TIME" >> log/client_down.log
 fi
